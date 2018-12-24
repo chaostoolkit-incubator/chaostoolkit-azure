9f2ea71be5ff38e77b25466e28bd577777cf9789
Derre, Bugra (415) <bugra.derre@daimler.com>
2018-12-24 14:22:58 +0100

    Introduce tests
    This commit introduces the first pytests for the Chaos Toolkit Azure
    extension.
    
    Resolves: #39
    Signed-off-by: Derre, Bugra (415) <bugra.derre@daimler.com>

--- chaosazure/vmss/actions.py
+++ chaosazure/vmss/actions.py
@@ -6,7 +6,9 @@ from chaoslib.exceptions import FailedActivity
 from logzero import logger
 
 from chaosazure import auth
-from chaosazure.vmss.vmss_fetcher import fetch_vmss, fetch_vmss_instances
+from chaosazure.rgraph.resource_graph import fetch_resources
+from chaosazure.vmss.constants import RES_TYPE_VMSS
+from chaosazure.vmss.vmss_fetcher import fetch_vmss_instances
 
 __all__ = ["delete_vmss", "restart_vmss", "stop_vmss", "deallocate_vmss"]
 
@@ -33,28 +35,17 @@ def delete_vmss(filter: str = None,
         "Start delete_vmss: configuration='{}', filter='{}'".format(
             configuration, filter))
 
-    vmss = fetch_vmss(filter, configuration, secrets)
-
-    if not vmss:
-        logger.warn("No virtual machine scale sets found")
-        raise FailedActivity("No virtual machine scale sets found")
-
-    choice_vmss = random.choice(vmss)
-    vmss_instances = fetch_vmss_instances(choice_vmss, configuration, secrets)
-
-    if not vmss_instances:
-        logger.warn("No virtual machine scale set instances found")
-        raise FailedActivity("No virtual machine scale set instances found")
-
-    choice_vmss_instance = random.choice(vmss_instances)
-    client = init_client(secrets, configuration)
+    vmss = choose_vmss_at_random(filter, configuration, secrets)
+    vmss_instance = choose_vmss_instance_at_random(
+        vmss, configuration, secrets)
 
     logger.debug(
-        "Deleting instance: {}".format(choice_vmss_instance['name']))
+        "Deleting instance: {}".format(vmss_instance['name']))
+    client = init_client(secrets, configuration)
     client.virtual_machine_scale_set_vms.delete(
-        choice_vmss['resourceGroup'],
-        choice_vmss['name'],
-        choice_vmss_instance['instanceId'])
+        vmss['resourceGroup'],
+        vmss['name'],
+        vmss_instance['instanceId'])
 
 
 def restart_vmss(filter: str = None,
@@ -74,35 +65,24 @@ def restart_vmss(filter: str = None,
     logger.debug(
         "Start restart_vmss: configuration='{}', filter='{}'".format(
             configuration, filter))
-    vmss = fetch_vmss(filter, configuration, secrets)
-
-    if not vmss:
-        logger.warn("No virtual machine scale sets found")
-        raise FailedActivity("No virtual machine scale sets found")
-
-    choice_vmss = random.choice(vmss)
-    vmss_instances = fetch_vmss_instances(choice_vmss, configuration, secrets)
-
-    if not vmss_instances:
-        logger.warn("No virtual machine scale set instances found")
-        raise FailedActivity("No virtual machine scale set instances found")
-
-    choice_vmss_instance = random.choice(vmss_instances)
-    client = init_client(secrets, configuration)
+    vmss = choose_vmss_at_random(filter, configuration, secrets)
+    vmss_instance = choose_vmss_instance_at_random(
+        vmss, configuration, secrets)
 
     logger.debug(
-        "Restarting instance: {}".format(choice_vmss_instance['name']))
+        "Restarting instance: {}".format(vmss_instance['name']))
+    client = init_client(secrets, configuration)
     client.virtual_machine_scale_set_vms.restart(
-        choice_vmss['resourceGroup'],
-        choice_vmss['name'],
-        choice_vmss_instance['instanceId'])
+        vmss['resourceGroup'],
+        vmss['name'],
+        vmss_instance['instanceId'])
 
 
 def stop_vmss(filter: str = None,
               configuration: Configuration = None,
               secrets: Secrets = None):
     """
-    Power off a virtual machine scale set instance at random.
+    Stop a virtual machine scale set instance at random.
      Parameters
     ----------
     filter : str
@@ -115,28 +95,18 @@ def stop_vmss(filter: str = None,
     logger.debug(
         "Start stop_vmss: configuration='{}', filter='{}'".format(
             configuration, filter))
-    vmss = fetch_vmss(filter, configuration, secrets)
 
-    if not vmss:
-        logger.warn("No virtual machine scale sets found")
-        raise FailedActivity("No virtual machine scale sets found")
-
-    choice_vmss = random.choice(vmss)
-    vmss_instances = fetch_vmss_instances(choice_vmss, configuration, secrets)
-
-    if not vmss_instances:
-        logger.warn("No virtual machine scale set instances found")
-        raise FailedActivity("No virtual machine scale set instances found")
-
-    choice_vmss_instance = random.choice(vmss_instances)
-    client = init_client(secrets, configuration)
+    vmss = choose_vmss_at_random(filter, configuration, secrets)
+    vmss_instance = choose_vmss_instance_at_random(
+        vmss, configuration, secrets)
 
     logger.debug(
-        "Stopping instance: {}".format(choice_vmss_instance['name']))
+        "Stopping instance: {}".format(vmss_instance['name']))
+    client = init_client(secrets, configuration)
     client.virtual_machine_scale_set_vms.power_off(
-        choice_vmss['resourceGroup'],
-        choice_vmss['name'],
-        choice_vmss_instance['instanceId'])
+        vmss['resourceGroup'],
+        vmss['name'],
+        vmss_instance['instanceId'])
 
 
 def deallocate_vmss(filter: str = None,
@@ -156,33 +126,47 @@ def deallocate_vmss(filter: str = None,
     logger.debug(
         "Start deallocate_vmss: configuration='{}', filter='{}'".format(
             configuration, filter))
-    vmss = fetch_vmss(filter, configuration, secrets)
-
-    if not vmss:
-        logger.warn("No virtual machine scale sets found")
-        raise FailedActivity("No virtual machine scale sets found")
-
-    choice_vmss = random.choice(vmss)
-    vmss_instances = fetch_vmss_instances(choice_vmss, configuration, secrets)
-
-    if not vmss_instances:
-        logger.warn("No virtual machine scale set instances found")
-        raise FailedActivity("No virtual machine scale set instances found")
-
-    choice_vmss_instance = random.choice(vmss_instances)
-    client = init_client(secrets, configuration)
+    vmss = choose_vmss_at_random(filter, configuration, secrets)
+    vmss_instance = choose_vmss_instance_at_random(
+        vmss, configuration, secrets)
 
     logger.debug(
-        "Deallocating instance: {}".format(choice_vmss_instance['name']))
+        "Deallocating instance: {}".format(vmss_instance['name']))
+    client = init_client(secrets, configuration)
     client.virtual_machine_scale_set_vms.deallocate(
-        choice_vmss['resourceGroup'],
-        choice_vmss['name'],
-        choice_vmss_instance['instanceId'])
+        vmss['resourceGroup'],
+        vmss['name'],
+        vmss_instance['instanceId'])
 
 
 ###############################################################################
 # Private helper functions
 ###############################################################################
+def choose_vmss_instance_at_random(vmss_choice, configuration, secrets):
+    vmss_instances = fetch_vmss_instances(vmss_choice, configuration, secrets)
+    if not vmss_instances:
+        logger.warning("No virtual machine scale set instances found")
+        raise FailedActivity("No virtual machine scale set instances found")
+    else:
+        logger.debug(
+            "Found virtual machine scale set instances: {}".format(
+                [x['name'] for x in vmss_instances]))
+    choice_vmss_instance = random.choice(vmss_instances)
+    return choice_vmss_instance
+
+
+def choose_vmss_at_random(filter, configuration, secrets):
+    vmss = fetch_resources(filter, RES_TYPE_VMSS, secrets, configuration)
+    if not vmss:
+        logger.warning("No virtual machine scale sets found")
+        raise FailedActivity("No virtual machine scale sets found")
+    else:
+        logger.debug(
+            "Found virtual machine scale sets: {}".format(
+                [x['name'] for x in vmss]))
+    return random.choice(vmss)
+
+
 def init_client(secrets, configuration):
     with auth(secrets) as cred:
         subscription_id = configuration['azure']['subscription_id']
---
+++ tests/aks/test_aks_actions.py
@@ -0,0 +1,70 @@
+from unittest.mock import patch
+
+import pytest
+from chaoslib.exceptions import FailedActivity
+
+from chaosazure.aks.actions import restart_node, stop_node, delete_node
+
+resource = {
+    'name': 'chaos-aks',
+    'resourceGroup': 'rg',
+    'properties': {
+        'nodeResourceGroup': 'nrg'
+    }}
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+@patch('chaosazure.aks.actions.delete_machine', autospec=True)
+def test_delete_node(delete, fetch):
+    resource_list = [resource]
+    fetch.return_value = resource_list
+
+    delete_node(None, None, None)
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_restart_node_with_no_nodes(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        delete_node(None, None, None)
+
+    assert "No AKS clusters found" in str(x)
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+@patch('chaosazure.aks.actions.stop_machine', autospec=True)
+def test_stop_node(stop, fetch):
+    resource_list = [resource]
+    fetch.return_value = resource_list
+
+    stop_node(None, None, None)
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_restart_node_with_no_nodes(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        stop_node(None, None, None)
+
+    assert "No AKS clusters found" in str(x)
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+@patch('chaosazure.aks.actions.restart_machine', autospec=True)
+def test_restart_node(restart, fetch):
+    resource_list = [resource]
+    fetch.return_value = resource_list
+
+    restart_node(None, None, None)
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_restart_node_with_no_nodes(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        restart_node(None, None, None)
+
+    assert "No AKS clusters found" in str(x)
