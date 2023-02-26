eb475d4513c2fa3b5b6c6b0ae7ad41342ab4d2af
Sylvain Hellegouarch <sh@defuze.org>
2023-02-26 22:34:12 +0100

    fix setuptools syntax
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>
    Signed-off-by: dcn <davidcoudron@hotmail.fr>

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup_params = dict(
     install_requires=install_require,
     tests_require=test_require,
     setup_requires=pytest_runner,
-    python_requires='>=3.7.*'
+    python_requires='>=3.7'
 )
 
 
