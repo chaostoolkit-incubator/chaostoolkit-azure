163339a4f5433281521045fe3a1d0e0431ddadfd
Jean-Baptiste Blache <53554866+jbblache@users.noreply.github.com>
2023-04-24 20:58:40 +0200

    Added AKS cluster actions and probes (#150)
    * Added AKS cluster operation
    * Added Probes for AKS cluster
    
    Signed-off-by: jbblache <jbaptiste.blache@gmail.com>
    Signed-off-by: dcn <davidcoudron@hotmail.fr>

--- tests/aks/test_aks_actions.py
+++ tests/aks/test_aks_actions.py
@@ -1,9 +1,9 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch, mock_open
 
 import pytest
 from chaoslib.exceptions import FailedActivity
 
-from chaosazure.aks.actions import restart_node, stop_node, delete_node
+from chaosazure.aks.actions import restart_node, stop_node, delete_node, start_managed_clusters, stop_managed_clusters, delete_managed_clusters
 
 resource = {
     'name': 'chaos-aks',
@@ -11,6 +11,33 @@ resource = {
     'properties': {
         'nodeResourceGroup': 'nrg'
     }}
+
+CONFIG = {
+    "azure": {
+        "subscription_id": "***REMOVED***"
+    }
+}
+
+SECRETS = {
+    "client_id": "***REMOVED***",
+    "client_secret": "***REMOVED***",
+    "tenant_id": "***REMOVED***"
+}
+
+SECRETS_CHINA = {
+    "client_id": "***REMOVED***",
+    "client_secret": "***REMOVED***",
+    "tenant_id": "***REMOVED***",
+    "azure_cloud": "AZURE_CHINA_CLOUD"
+}
+
+MANAGED_CLUSTER_ALPHA = {
+    'name': 'ManagedClusterAlpha',
+    'resourceGroup': 'group'}
+
+MANAGED_CLUSTER_BETA = {
+    'name': 'ManagedClusterBeta',
+    'resourceGroup': 'group'}
 
 
 @patch('chaosazure.aks.actions.fetch_resources', autospec=True)
@@ -68,3 +95,145 @@ def test_restart_node_with_no_nodes(fetch):
         restart_node(None, None, None)
 
     assert "No AKS clusters found" in str(x.value)
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_delete_one_managed_cluster(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 1"
+    delete_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_delete.call_count == 1
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_delete_one_MANAGED_CLUSTER_china(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 1"
+    delete_managed_clusters(f, CONFIG, SECRETS_CHINA)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS_CHINA)
+    assert client.managed_clusters.begin_delete.call_count == 1
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_delete_two_managed_clusters(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA, MANAGED_CLUSTER_BETA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 2"
+    delete_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_delete.call_count == 2
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_delete_MANAGED_CLUSTER_with_no_managed_clusters(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        delete_managed_clusters(None, None, None)
+
+    assert "No Managed Clusters found" in str(x.value)
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_start_one_managed_cluster(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 1"
+    start_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_start.call_count == 1
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_start_two_managed_clusters(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA, MANAGED_CLUSTER_BETA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 2"
+    start_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_start.call_count == 2
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_start_MANAGED_CLUSTER_with_no_managed_clusters(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        start_managed_clusters(None, None, None)
+
+    assert "No Managed Clusters found" in str(x.value)
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_stop_one_managed_cluster(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 1"
+    stop_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_stop.call_count == 1
+
+
+@patch('chaosazure.aks.actions.__fetch_managed_clusters', autospec=True)
+@patch('chaosazure.aks.actions.__containerservice_mgmt_client', autospec=True)
+def test_stop_two_managed_clusters(init, fetch):
+    client = MagicMock()
+    init.return_value = client
+
+    managed_clusters = [MANAGED_CLUSTER_ALPHA, MANAGED_CLUSTER_BETA]
+    fetch.return_value = managed_clusters
+
+    f = "where resourceGroup=='myresourcegroup' | sample 2"
+    stop_managed_clusters(f, CONFIG, SECRETS)
+
+    fetch.assert_called_with(f, CONFIG, SECRETS)
+    assert client.managed_clusters.begin_stop.call_count == 2
+
+
+@patch('chaosazure.aks.actions.fetch_resources', autospec=True)
+def test_stop_MANAGED_CLUSTER_with_no_managed_clusters(fetch):
+    with pytest.raises(FailedActivity) as x:
+        resource_list = []
+        fetch.return_value = resource_list
+        stop_managed_clusters(None, None, None)
+
+    assert "No Managed Clusters found" in str(x.value)
