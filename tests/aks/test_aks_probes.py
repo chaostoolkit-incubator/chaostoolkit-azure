527a40b101a22d842dac9105b6a9b68c77093c7f
Jean-Baptiste Blache <53554866+jbblache@users.noreply.github.com>
2023-04-24 20:58:40 +0200

    Added AKS cluster actions and probes (#150)
    * Added AKS cluster operation
    * Added Probes for AKS cluster
    
    Signed-off-by: jbblache <jbaptiste.blache@gmail.com>

---
+++ tests/aks/test_aks_probes.py
@@ -0,0 +1,34 @@
+from unittest.mock import MagicMock, patch, mock_open
+
+
+from chaosazure.aks.probes import count_managed_clusters, describe_managed_clusters
+from logzero import logger
+
+
+resource = {
+    'name': 'chaos-managed_cluster',
+    'resourceGroup': 'rg'}
+
+
+@patch('chaosazure.aks.probes.fetch_resources', autospec=True)
+def test_count_managed_clusters(fetch):
+    resource_list = [resource]
+    fetch.return_value = resource_list
+
+    count = count_managed_clusters(None, None)
+
+    assert count == 1
+
+
+@patch('chaosazure.aks.probes.fetch_resources', autospec=True)
+def test_describe_managed_clusters(fetch):
+    resource_list = [resource]
+    fetch.return_value = resource_list
+
+    description = describe_managed_clusters(None, None)
+
+    assert description[0]['name'] == resource['name']
+    assert description[0]['resourceGroup'] == resource['resourceGroup']
+
+
+
