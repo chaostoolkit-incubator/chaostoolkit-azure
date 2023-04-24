b9aa01a0a19b3c620e3d63365610c5d54a72ae6d
Jean-Baptiste Blache <53554866+jbblache@users.noreply.github.com>
2023-04-24 20:58:40 +0200

    Added AKS cluster actions and probes (#150)
    * Added AKS cluster operation
    * Added Probes for AKS cluster
    
    Signed-off-by: jbblache <jbaptiste.blache@gmail.com>
    Signed-off-by: dcn <davidcoudron@hotmail.fr>

--- chaosazure/__init__.py
+++ chaosazure/__init__.py
@@ -5,6 +5,7 @@
 from typing import List
 
 from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.containerservice import ContainerServiceClient
 from azure.mgmt.rdbms.postgresql_flexibleservers import (
     PostgreSQLManagementClient as PostgreSQLFlexibleManagementClient
 )
@@ -57,6 +58,27 @@ def init_compute_management_client(
         base_url = secrets.get('cloud').endpoints.resource_manager
         scopes = [base_url + "/.default"]
         client = ComputeManagementClient(
+            credential=authentication,
+            credential_scopes=scopes,
+            subscription_id=configuration.get('subscription_id'),
+            base_url=base_url)
+
+        return client
+
+
+def init_containerservice_management_client(
+        experiment_secrets: Secrets,
+        experiment_configuration: Configuration) -> ContainerServiceClient:
+    """
+    Initializes Container Service management client for managed clusters
+    resources under Azure Resource manager.
+    """
+    secrets = load_secrets(experiment_secrets)
+    configuration = load_configuration(experiment_configuration)
+    with auth(secrets) as authentication:
+        base_url = secrets.get('cloud').endpoints.resource_manager
+        scopes = [base_url + "/.default"]
+        client = ContainerServiceClient(
             credential=authentication,
             credential_scopes=scopes,
             subscription_id=configuration.get('subscription_id'),
@@ -217,6 +239,7 @@ def __load_exported_activities() -> List[DiscoveredActivities]:
     activities.extend(discover_actions("chaosazure.machine.actions"))
     activities.extend(discover_probes("chaosazure.machine.probes"))
     activities.extend(discover_actions("chaosazure.aks.actions"))
+    activities.extend(discover_probes("chaosazure.aks.probes"))
     activities.extend(discover_actions("chaosazure.vmss.actions"))
     activities.extend(discover_actions("chaosazure.webapp.actions"))
     activities.extend(discover_probes("chaosazure.webapp.probes"))
