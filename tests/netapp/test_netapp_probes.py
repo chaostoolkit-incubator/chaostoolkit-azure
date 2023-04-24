5b7605e98d28df7686f476d8d2a32a651a488cc8
Jean-Baptiste Blache <53554866+jbblache@users.noreply.github.com>
2023-04-24 11:42:36 +0200

    Added Azure storage actions and probes (#149)
    * Added Azure storage actions and probes
    * Add azure-mgmt-storage in requirements
    
    Signed-off-by: jbblache <jbaptiste.blache@gmail.com>
    Signed-off-by: dcn <davidcoudron@hotmail.fr>

--- tests/netapp/test_netapp_probes.py
+++ tests/netapp/test_netapp_probes.py
@@ -1,41 +1,35 @@
 from unittest.mock import MagicMock, patch, mock_open
 
 
-from chaosazure.application_gateway.probes import count_application_gateways, describe_application_gateways, describe_routes
-from data import application_gateway_provider
+from chaosazure.netapp.probes import count_netapp_volumes, describe_netapp_volumes
+from data import netapp_provider
 from logzero import logger
 
 
-resource = {
-    'name': 'chaos-application_gateway',
-    'resourceGroup': 'rg'}
+NETAPP_VOLUME_ALPHA = {
+    'name': 'NetAppAccount/NetAppPoolName/NetAppVolumeAlpha',
+    'resourceGroup': 'group'}
 
-route_resource = {
-    "name": "appgwrule",
-    "id": "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/applicationGateways/appgw/requestRoutingRules/appgwrule",
-    "properties": {
-        "provisioningState": "Succeeded",
-        "ruleType": "Basic"
-    }
-}
+NETAPP_VOLUME_BETA = {
+    'name': 'NetAppAccount/NetAppPoolName/NetAppVolumeBeta',
+    'resourceGroup': 'group'}
 
-
-@patch('chaosazure.application_gateway.probes.fetch_resources', autospec=True)
-def test_count_application_gateways(fetch):
-    resource_list = [resource]
+@patch('chaosazure.netapp.probes.fetch_resources', autospec=True)
+def test_count_netapp_volumes(fetch):
+    resource_list = [NETAPP_VOLUME_ALPHA, NETAPP_VOLUME_BETA]
     fetch.return_value = resource_list
 
-    count = count_application_gateways(None, None)
+    count = count_netapp_volumes(None, None)
 
-    assert count == 1
+    assert count == 2
 
 
-@patch('chaosazure.application_gateway.probes.fetch_resources', autospec=True)
-def test_describe_application_gateways(fetch):
-    resource_list = [resource]
+@patch('chaosazure.netapp.probes.fetch_resources', autospec=True)
+def test_describe_netapp_volumes(fetch):
+    resource_list = [NETAPP_VOLUME_ALPHA]
     fetch.return_value = resource_list
 
-    description = describe_application_gateways(None, None)
+    description = describe_netapp_volumes(None, None)
 
-    assert description[0]['name'] == resource['name']
-    assert description[0]['resourceGroup'] == resource['resourceGroup']
+    assert description[0]['name'] == resource_list[0]['name']
+    assert description[0]['resourceGroup'] == resource_list[0]['resourceGroup']
