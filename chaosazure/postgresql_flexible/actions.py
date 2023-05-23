0e5eb985ef0d648f11873b3e347589c5a9913379
dcn <davidcoudron@hotmail.fr>
2023-05-23 10:00:51 +0200

    Update actions.py
    Signed-off-by: dcn <davidcoudron@hotmail.fr>

--- chaosazure/postgresql_flexible/actions.py
+++ chaosazure/postgresql_flexible/actions.py
@@ -20,7 +20,7 @@ from chaosazure.common.resources.graph import fetch_resources
 __all__ = ["delete_servers", "stop_servers", "restart_servers",
            "start_servers", "delete_databases", "create_databases", "delete_tables"]
 
-from chaosazure.vmss.records import Records           
+from chaosazure.vmss.records import Records
 
 
 def delete_servers(filter: str = None,
@@ -201,6 +201,114 @@ def start_servers(filter: str = None,
 
     return server_records.output_as_dict('resources')
 
+
+def delete_databases(filter: str = None,
+                     name_pattern: str = None,
+                     configuration: Configuration = None,
+                     secrets: Secrets = None):
+    """
+    Delete databases at random.
+
+    **Be aware**: Deleting a database is an invasive action. You will not be
+    able to recover the database once you deleted it.
+
+    Parameters
+    ----------
+    filter : str, optional
+        Filter the servers. If the filter is omitted all databases
+        of all servers in the subscription will be selected
+        as potential chaos candidates.
+
+    name_pattern : str, optional
+        Filter the databases. If the filter is omitted all databases in
+        the server will be selected for the probe.
+        Pattern example:
+        'app[0-9]{3}'
+
+    Examples
+    --------
+    Some calling examples. Deep dive into the filter syntax:
+    https://docs.microsoft.com/en-us/azure/kusto/query/
+
+    >>> delete_databases("where resourceGroup=='rg'", 'chaos-*', c, s)
+    Delete all database named 'chaos-*' in all servers from the group 'rg'
+
+    >>> delete_databases("where resourceGroup=='rg' and name='name'", 'chaos-test', c, s)
+    Delete all database named 'chaos-*' the server from the group 'rg' having the name 'name'
+    """
+    logger.debug(
+        "Start delete_databases: "
+        "configuration='{}', filter='{}', name_pattern='{}'".format(
+            configuration, filter, name_pattern))
+
+    pattern = None
+    if name_pattern:
+        pattern = re.compile(name_pattern)
+
+    servers = __fetch_servers(filter, configuration, secrets)
+    client = __postgresql_flexible_mgmt_client(secrets, configuration)
+    database_records = Records()
+    for s in servers:
+        group = s['resourceGroup']
+        server_name = s['name']
+
+        for d in client.databases.list_by_server(group, server_name):
+            name = d.name
+            if pattern is None or pattern.search(name):
+                client.databases.begin_delete(group, server_name, name)
+                database_records.add(cleanse.database_database(d.as_dict()))
+
+        logger.debug("Deleting database: {}/{}".format(server_name, name))
+
+    return database_records.output_as_dict('resources')
+
+
+def create_databases(filter: str = None,
+                     name: str = None,
+                     charset: str = None,
+                     collation: str = None,
+                     configuration: Configuration = None,
+                     secrets: Secrets = None):
+    """
+    Delete databases at random.
+
+    **Be aware**: Deleting a database is an invasive action. You will not be
+    able to recover the database once you deleted it.
+
+    Parameters
+    ----------
+    filter : str, optional
+        Filter the servers. If the filter is omitted all databases
+        of all servers in the subscription will be selected
+        as potential chaos candidates.
+
+    name : str, required
+        The name of the database to create.
+
+    Examples
+    --------
+    Some calling examples. Deep dive into the filter syntax:
+    https://docs.microsoft.com/en-us/azure/kusto/query/
+
+    >>> create_databases("where resourceGroup=='rg'", 'chaos-test', c, s)
+    Creating database named 'chaos-test' in all servers from the group 'rg'
+
+    >>> create_databases("where resourceGroup=='rg' and name='name'", 'chaos-test', c, s)
+    Creating database named 'chaos-test' the server from the group 'rg' having the name 'name'
+    """
+    logger.debug(
+        "Start create_databases: "
+        "configuration='{}', filter='{}', name='{}'".format(configuration, filter, name))
+
+    servers = __fetch_servers(filter, configuration, secrets)
+    client = __postgresql_flexible_mgmt_client(secrets, configuration)
+    database_parameters = Database(charset=charset, collation=collation)
+    for s in servers:
+        group = s['resourceGroup']
+        server_name = s['name']
+
+        client.databases.begin_create(group, server_name, name, database_parameters)
+
 def delete_tables(filter: str = None,
                   table_name: str = None,
                   configuration: Configuration = None,
@@ -331,7 +439,7 @@ def delete_tables(filter: str = None,
                     f"Failed to connect to database '{dbname}' on server '{server_name}'")
                 continue
 
-    return server_records.output_as_dict('resources')
+    return server_records.output_as_dict('resources')        
 
 
 ###############################################################################
@@ -363,4 +471,3 @@ def __fetch_servers(filter, configuration, secrets) -> []:
 
 def __postgresql_flexible_mgmt_client(secrets, configuration):
     return init_postgresql_flexible_management_client(secrets, configuration)
-    
